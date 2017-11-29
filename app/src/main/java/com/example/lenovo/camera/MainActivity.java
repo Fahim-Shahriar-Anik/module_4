036c27517aa33e2d256dc7459c8342c4524177d7
Fahim-Shahriar-Anik <fsanik97@gmail.com>
2017-11-30 00:28:30 +0600

    tewst

---
+++ app/src/main/java/com/example/lenovo/camera/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.lenovo.camera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements camera {
+
+    ImageView result;
+    static final int REQUEST_IMAGE_CAPTURE=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button click=(Button) findViewById(R.id.camera);
+        result= (ImageView)findViewById(R.id.imageView);
+    }
+
+    public void dispatchTakePic(View view){
+        Intent takepic=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takepic.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takepic,REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+    protected void onActivityResult(int resultCode, Intent data){
+        Bundle extras = data.getExtras();
+        Bitmap imageBitmap=(Bitmap) extras.get("data");
+        result.setImageBitmap(imageBitmap);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
